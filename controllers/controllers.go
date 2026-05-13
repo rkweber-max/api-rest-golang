8a38efd0eaf1fe1a0d509b07a1b7d365fdec01c1
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-05-13 19:00:41 -0300

    feature(personality) add CRUD operations for personality management in controllers and update routes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -23,9 +23,36 @@ func AllPersonalities(w http.ResponseWriter, r *http.Request) {
 func GitById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-
 	var p models.Personality
 	database.DB.First(&p, id)
 
 	json.NewEncoder(w).Encode(p)
+}
+
+func CreatePersonality(w http.ResponseWriter, r *http.Request) {
+	var personality models.Personality
+	json.NewDecoder(r.Body).Decode(&personality)
+	database.DB.Create(&personality)
+
+	json.NewEncoder(w).Encode(&personality)
+}
+
+func DeletePersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var p models.Personality
+	database.DB.Delete(&p, id)
+
+	json.NewEncoder(w).Encode(&p)
+}
+
+func EditPersonality(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var p models.Personality
+	database.DB.First(&p, id)
+
+	json.NewDecoder(r.Body).Decode(&p)
+	database.DB.Save(&p)
+	json.NewEncoder(w).Encode(&p)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -12,6 +12,9 @@ func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("GET")
+	r.HandleFunc("/api/personalities", controllers.CreatePersonality).Methods("POST")
 	r.HandleFunc("/api/personalities/{id}", controllers.GitById).Methods("GET")
+	r.HandleFunc("/api/personalities/{id}", controllers.DeletePersonality).Methods("DELETE")
+	r.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8090", r))
 }
