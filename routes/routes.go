1fe3c28a77eb2afa61fdf87cb200d2fae4b2e763
Rodrigo Kunzler Weber <rodrigo.weber@appmax.com.br>
2026-05-19 18:21:21 -0300

    chore(dependencies) add httpsnoop and handlers packages, implement ContentTypeMiddleware, and update routes for CORS support

--- routes/routes.go
+++ routes/routes.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rkweber-max/api-rest-golang/controllers"
+	"github.com/rkweber-max/api-rest-golang/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("GET")
 	r.HandleFunc("/api/personalities", controllers.CreatePersonality).Methods("POST")
 	r.HandleFunc("/api/personalities/{id}", controllers.GitById).Methods("GET")
 	r.HandleFunc("/api/personalities/{id}", controllers.DeletePersonality).Methods("DELETE")
 	r.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8090", r))
+	log.Fatal(http.ListenAndServe(":8090", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
